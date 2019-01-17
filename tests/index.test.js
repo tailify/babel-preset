0c9b17d95716c596b1e35d58db4396b559db6a30
Jamie Connolly <jamie.connolly@tailify.com>
2019-01-17 10:07:16 +0000

    Compile against the current node version by default

--- tests/index.test.js
+++ tests/index.test.js
@@ -2,13 +2,31 @@
 
 const babel = require('@babel/core');
 
+const source = 'let b = 1; a ** b;';
+
 test('should not contain invalid rules', () => {
-  const result = babel.transform('let b = 1; a ** b;', {
+  const result = babel.transform(source, {
     babelrc: false,
-    presets: ['./lib/index.js'],
+    presets: [['./lib/index.js']],
     sourceType: 'script',
   });
-
   expect(result).toBeTruthy();
+});
+
+test('should compile targeting the current node version by default', () => {
+  const result = babel.transform(source, {
+    babelrc: false,
+    presets: [['./lib/index.js']],
+    sourceType: 'script',
+  });
+  expect(result.code).toBe('let b = 1;\na ** b;');
+});
+
+test('should compile targeting browsers', () => {
+  const result = babel.transform(source, {
+    babelrc: false,
+    presets: [['./lib/index.js', { browser: true }]],
+    sourceType: 'script',
+  });
   expect(result.code).toBe('var b = 1;\nMath.pow(a, b);');
 });

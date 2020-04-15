56e821fd46b7c581d99b03909ae3c9efb14f37ae
Vladimir Aleksiev <vladimir.aleksiev@tailify.com>
2020-04-15 17:21:51 +0300

    WIP

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,5 +2,20 @@
 
 module.exports = {
   extends: ['@tailify/legacy'],
+  overrides: [
+    {
+      extends: ['plugin:jest/recommended', 'plugin:jest/style'],
+      files: ['*.test.js'],
+      parserOptions: {
+        ecmaVersion: 6,
+      },
+    },
+  ],
   root: true,
+  env: {
+    browser: true,
+    node: true,
+    commonjs: true,
+    es6: true,
+  },
 };
